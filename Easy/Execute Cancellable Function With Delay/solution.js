7ad3823cfded3d2aa6e0c45151b7d881a2ef5f7a
FuadNabi <fuad.nebiyev95@mail.ru>
2023-06-09 11:44:13 +0400

    add challenge Execute Cancellable Function With Delay

---
+++ Easy/Execute Cancellable Function With Delay/solution.js
@@ -0,0 +1,9 @@
+var cancellable = function(fn, args, t) {
+  let timeoutId;
+
+  const cancelFn = () =>  clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(() => fn(...args), t);
+
+  return cancelFn;  
+};
