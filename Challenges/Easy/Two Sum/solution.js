93e4fbe846aebad4fa6160e005c30dd72e3eba73
FuadNabi <fuad.nebiyev95@mail.ru>
2023-09-20 11:13:22 +0400

    Modify solution of Two Sum

--- Challenges/Easy/Two Sum/solution.js
+++ Challenges/Easy/Two Sum/solution.js
@@ -1,15 +1,9 @@
 var twoSum = function(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-      let currentNumber = nums[i];
-      let secondNumber = target - currentNumber;
-      let secondNumberIndices = [];
-      for (let j = i + 1; j < nums.length; j++) {
-          if (nums[j] === secondNumber) {
-              secondNumberIndices.push(j);
-          }
-      }
-      if (secondNumberIndices.length > 0) {
-          return [i, secondNumberIndices[0]];
-      }
-  }
+    for( let i = 0; i < nums.length; i++){
+        for( let j = i + 1; j < nums.length; j++ ) {
+            if(nums[i] + nums [j] === target) {
+                return [i, j]
+            }
+        }
+    }
 };
