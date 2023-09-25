de6f15b71b84600ed61422ddd203b9a7d95c027b
FuadNabi <fuad.nebiyev95@mail.ru>
2023-09-25 11:57:20 +0400

    Fix lintern errors

--- Challenges/Easy/Designer PDF Viewer/solution.js
+++ Challenges/Easy/Designer PDF Viewer/solution.js
@@ -3,15 +3,18 @@ function designerPdfViewer(h, word) {
   const letters = 'abcdefghijklmnopqrstuvwxyz';
 
   for (let i = 0; i < 26; i++) {
-  letterHeights[letters[i]] = h[i];
+    letterHeights[letters[i]] = h[i];
   }
-  let maxHeight = 0;
-  for (let i = 0; i < word.length; i++) {
-  const char = word[i];
-  const height = letterHeights[char];
-  maxHeight = Math.max(maxHeight, height);
-}
 
-return maxHeight * word.length;
+  let maxHeight = 0;
+
+  for (let i = 0; i < word.length; i++) {
+
+    const char = word[i];
+    const height = letterHeights[char];
+    maxHeight = Math.max(maxHeight, height);
+  }
+
+  return maxHeight * word.length;
 
 }
