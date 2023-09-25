2aa8071e6822480598f5773a784f229661373754
FuadNabi <fuad.nebiyev95@mail.ru>
2023-09-25 11:56:10 +0400

    Add Challenge Designer PDF Viewer

---
+++ Challenges/Easy/Designer PDF Viewer/solution.js
@@ -0,0 +1,17 @@
+function designerPdfViewer(h, word) {
+  const letterHeights = {};
+  const letters = 'abcdefghijklmnopqrstuvwxyz';
+
+  for (let i = 0; i < 26; i++) {
+  letterHeights[letters[i]] = h[i];
+  }
+  let maxHeight = 0;
+  for (let i = 0; i < word.length; i++) {
+  const char = word[i];
+  const height = letterHeights[char];
+  maxHeight = Math.max(maxHeight, height);
+}
+
+return maxHeight * word.length;
+
+}
